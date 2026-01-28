4c11873860c043c605562010d02abb5f01049c68
fl1mtDev <flmtdev@gmail.com>
2026-01-29 01:35:30 +0500

    fixed SecurityConfig

--- src/main/java/com/marketplace/config/SecurityConfig.java
+++ src/main/java/com/marketplace/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/auth/**",
+                                "/api/v1/auth/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
