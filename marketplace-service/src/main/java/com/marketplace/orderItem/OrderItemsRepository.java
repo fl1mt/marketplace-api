dffb958f3bb5f92e4e15ea87c084f19fe5a3ba20
fl1mtDev <flmtdev@gmail.com>
2026-02-13 03:51:16 +0500

    refactor: delete repository method

--- marketplace-service/src/main/java/com/marketplace/orderItem/OrderItemsRepository.java
+++ marketplace-service/src/main/java/com/marketplace/orderItem/OrderItemsRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface OrderItemsRepository extends JpaRepository<OrderItem, UUID> {
-    List<OrderItem> findAllByOrderId(UUID orderId);
     @Query("SELECT oi FROM OrderItem oi JOIN FETCH oi.product WHERE oi.order.id = :orderId")
     List<OrderItem> findByOrderIdWithProducts(@Param("orderId") UUID orderId);
     @Query("SELECT oi FROM OrderItem oi JOIN FETCH oi.product WHERE oi.order.id IN :orderIds")
